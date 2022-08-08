0999aaf484f5c03e6a2c052bb618c305b170de0c
Kent <kenttechnical@gmail.com>
2022-08-08 16:59:30 +0200

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+const key = 'UiGTGmssIlmchGjr5c6zbit4PS714Cd1'
+
+const getCity = async (location) =>{
+
+    const baseEndpoint = "http://dataservice.accuweather.com/locations/v1/cities/search"
+    const query = `?apikey=${key}&q=${location}`
+
+    const response = await fetch(baseEndpoint + query)
+    const data = await response.json()
+
+    return data[0]
+
+}
+
+const getCurrentConditions = async(locationkey) =>{
+    const endpoint = `http://dataservice.accuweather.com/currentconditions/v1/${locationkey}?apikey=${key}`
+
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    console.log(data)
+}
+
+
+getCity("Durban")
+    .then(data=>getCurrentConditions(data.Key))
+    .catch(error=>console.log(error))
