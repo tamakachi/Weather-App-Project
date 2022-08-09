537517a499f330f571d6ead9fee11493a24f2d68
Kent <kenttechnical@gmail.com>
2022-08-09 17:32:45 +0200

    UI displays API response after submit

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const cityInput = document.querySelector(".change-location")
-
+const details = document.querySelector(".details")
+const card = document.querySelector(".card")
 const updateCity = async(city) =>{
 
     const cityData = await getCity(city)
@@ -19,7 +20,23 @@ cityInput.addEventListener("submit", e =>{
     cityInput.reset()
 
     updateCity(city)
-        .then(data=>console.log(data))
+        .then(data=>updateUI(data))
         .catch(err=>console.log(err))
     
 })
+
+const updateUI = (data) =>{
+
+    details.innerHTML = `
+    <h5 class="my-3">${data.cityData.EnglishName}</h5>
+    <div class="my-3">${data.weatherData[0].WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>temp</span>
+        <span>${data.weatherData[0].Temperature.Metric.Value}&deg;</span>
+    </div>
+    `
+
+    if (card.classList.contains("d-none"))
+    {card.classList.remove("d-none")}
+    
+}
